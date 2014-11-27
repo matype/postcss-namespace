5d763cec1b86506671d51caa36380c3a3223bafd
Masaaki Morishita <ikasama3.0@gmail.com>
2014-11-27 19:19:44 +0900

    First implementation

--- index.js
+++ index.js
@@ -1,2 +1,62 @@
-module.exports = function () {
+var cssSelectorParser = require('css-selector-parser').CssSelectorParser;
+var selectorParser = new cssSelectorParser();
+var inspect = require('obj-inspector')
+
+module.exports = function plugin (options) {
+    options = options || {}
+
+    var preConnection = options.prefix || "-"
+    var sufConnection = options.suffix || "-"
+
+    return function (css) {
+        var newRules = []
+
+        css.eachAtRule(function (atRule, i) {
+            if (atRule.name === 'prefix') {
+                atRule.eachRule(function (rule) {
+                    if (isClassSelector(rule.selector)) {
+                        rule.selector = "." + atRule.params + preConnection + selectorParser.parse(rule.selector).rule.classNames[0]
+                    }
+                    if (isIdSelector(rule.selector)) {
+                        rule.selector = "#" + atRule.params + sufConnection + selectorParser.parse(rule.selector).rule.id
+                    }
+//                    inspect(rule)
+                    rule.eachDecl(function (decl) {
+                        decl.before = rule.before
+                        decl.after = rule.after
+                    })
+                    newRules.push(rule)
+                })
+
+            }
+
+            if (atRule.name === 'suffix') {
+                atRule.eachRule(function (rule) {
+                    if (isClassSelector(rule.selector)) {
+                        rule.selector = "." + selectorParser.parse(rule.selector).rule.classNames[0] + preConnection + atRule.params;
+                    }
+                    if (isIdSelector(rule.selector)) {
+                        rule.selector = "#" + selectorParser.parse(rule.selector).rule.id + sufConnection + atRule.params;
+                    }
+                    rule.eachDecl(function (decl) {
+                        decl.before = "\n  "
+                        decl.after = "\n  "
+                    })
+                    rule.after = "\n  "
+                    newRules.push(rule)
+                })
+
+            }
+            atRule.removeSelf()
+        })
+        css.append(newRules)
+    }
+}
+
+function isClassSelector (selector) {
+    if (selectorParser.parse(selector).rule.classNames) return true
+}
+
+function isIdSelector (selector) {
+    if (selectorParser.parse(selector).rule.id) return true
 }
--- test/index.js
+++ test/index.js
@@ -1,6 +1,30 @@
-var test = require('colored-tape')
-var postcss-namespace = require('..')
+var fs = require('fs')
+var test = require('tape')
+var postcss = require('postcss')
+var namespace = require('..')
 
-test('description', function (t) {
-  t.end()
+function input (name) {
+    return fs.readFileSync('test/fixtures/' + name + '.css', 'utf-8').trim()
+}
+
+function output (name) {
+    return fs.readFileSync('test/fixtures/' + name + '.out.css', 'utf-8').trim()
+}
+
+function process (name) {
+    return postcss().use(namespace()).process(input(name)).css.trim()
+}
+
+function assert (t, name) {
+    return t.equal(process(name), output(name))
+}
+
+test('prefix', function (t) {
+    assert(t, 'prefix')
+    t.end()
+})
+
+test('suffix', function (t) {
+    assert(t, 'suffix')
+    t.end()
 })
